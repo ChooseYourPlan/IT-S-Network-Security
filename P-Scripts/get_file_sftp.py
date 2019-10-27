c637b98a7a5a7dbe3768fde563ca380530aa3c79
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-27 03:01:56 +0100

    Added ConfigParser; Added Error Handling

--- P-Scripts/get_file_sftp.py
+++ P-Scripts/get_file_sftp.py
@@ -1,7 +1,23 @@
+import os
 import pysftp
+from configparser import ConfigParser
+
+parser = ConfigParser()
+if os.path.isfile('../config_payload.ini'):
+    parser.read('../config_payload.ini')
+elif os.path.isfile('config_payload.ini'):
+    parser.read('config_payload.ini')
+else:
+    raise SystemExit
 
 cnopt = pysftp.CnOpts()
 cnopt.hostkeys = None   
 
-with pysftp.Connection("weissenfels.tech",username="foo",password="pass",cnopts=cnopt, port=2222) as sftp: 
-    sftp.get("/CameraSample.apk")
+IP = parser.get('SFTP-Server','IP-Adress')
+uname = parser.get('SFTP-Server','Username')
+pword = parser.get('SFTP-Server','Password')
+sftp_port = parser.get('SFTP-Server','Port')
+filename = parser.get('Filename','Original')
+
+with pysftp.Connection(IP,username=uname,password=pword,cnopts=cnopt,port=int(sftp_port)) as sftp: 
+    sftp.get(filename)
