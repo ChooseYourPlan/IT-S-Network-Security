76838e2613b0b1b94503dc78948c512390d40b7b
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-30 23:38:20 +0100

    Added Port Configuration

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build . -t container1-payload-build
-sudo docker run -it --entrypoint "/usr/src/wdir/start_payload.sh" container1-payload-build
+sudo docker run -it -p 4444:4444 --entrypoint "/usr/src/wdir/start_payload.sh" container1-payload-build
