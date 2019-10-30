ddd7d58309148f233648dc2f523405d9a12632ee
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-30 21:22:16 +0000

    Fixed Bug in --entrypoint flag

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker build . -t container1-payload-build
-sudo docker run -it --entrypoint "sudo /usr/wdir/start_payload.sh" container1-payload-build
+sudo docker run -it --entrypoint "/usr/wdir/start_payload.sh" container1-payload-build
