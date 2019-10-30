aabedbd70485760dda1368984c30a187a5e619f5
root <timweissenfels7@gmail.com>
2019-10-30 18:44:47 +0000

    Added Path

--- P-Scripts/get_file_sftp.py
+++ P-Scripts/get_file_sftp.py
@@ -18,7 +18,8 @@ IP = parser.get('SFTP-Server','IP-Adress')
 uname = parser.get('SFTP-Server','Username')
 pword = parser.get('SFTP-Server','Password')
 sftp_port = parser.get('SFTP-Server','Port')
+sftp_path = parser.get('SFTP-Server','Path')
 filename = parser.get('Filename','Original')
 
 with pysftp.Connection(IP,username=uname,password=pword,cnopts=cnopt,port=int(sftp_port)) as sftp: 
-    sftp.get(filename)
+    sftp.get(sftp_path + filename)
