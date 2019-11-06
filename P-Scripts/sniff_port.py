ee0bfe021db01ba9695d77befaf44927c9d5b474
ChooseYourPlan <timweissenfels7@gmail.com>
2019-11-06 23:26:14 +0100

    Changed Port Range

--- P-Scripts/sniff_port.py
+++ P-Scripts/sniff_port.py
@@ -5,7 +5,8 @@ import sys
 import datetime
 address = []
 
-with subprocess.Popen(["python", "sniff_add.py"], stdout=subprocess.PIPE, bufsize=1,universal_newlines=True) as p:
+subp = subprocess.Popen(["python", "sniff_add.py"], stdout=subprocess.PIPE, bufsize=1,universal_newlines=True)
+with subp as p:
     for line in p.stdout:
         address.append(line)
 
@@ -16,7 +17,7 @@ t1 = datetime.datetime.now()
 try:
     for remoteServerIP in address:
         print(remoteServerIP.rstrip())
-        for port in range(1,1025):  
+        for port in range(1,525):  
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             result = sock.connect_ex((remoteServerIP.rstrip(), port))
             if result == 0:
@@ -30,6 +31,7 @@ except socket.gaierror:
 except socket.error:
     print("IP-Adresse nicht erreichbar")
     sys.exit()
+
 t2 = datetime.datetime.now()
 
 total = t2 - t1
